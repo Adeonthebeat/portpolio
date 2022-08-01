4cb930ef269391425a6e10fa85f27cfbfd41143c
Adeonthebeat <fowsdw@naver.com>
2022-08-01 23:18:09 +0900

    Initial commit

---
+++ src/main/java/com/ade/portpolio/WebController.java
@@ -0,0 +1,14 @@
+package com.ade.portpolio;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WebController {
+
+    @GetMapping("/index")
+    public String index() {
+        return "index";
+    }
+
+}
